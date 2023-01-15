1501979b35c202c475a6b567365316401ffb9040
Maria Mikhaylova <mihailova_ms@gkl-kemerovo.ru>
2023-01-15 12:08:14 +0300

    filter

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
-import api from "./api";
+import React, { useEffect, useState } from "react";
 import Users from "./components/users";
-import SearchStatus from "./components/searchStatus";
+import api from "./api";
 
 function App() {
-    const [users, setUsers] = useState(api.users.fetchAll());
+    const [users, setUsers] = useState();
+    useEffect(() => {
+        api.users.default.fetchAll().then((data) => setUsers(data));
+    }, []);
     const handleDelete = (userId) => {
-        const newUsers = users.filter((user) => user._id !== userId);
-        setUsers(newUsers);
+        setUsers(users.filter((user) => user._id !== userId));
     };
     const handToggleBookMark = (id) => {
         const newUsers = users.map((user) => {
@@ -21,12 +22,13 @@ function App() {
 
     return (
         <div>
-            <SearchStatus length={users.length} />
-            <Users
-                users={users}
-                onDelete={handleDelete}
-                onToggle={handToggleBookMark}
-            />
+            {users && (
+                <Users
+                    users={users}
+                    onDelete={handleDelete}
+                    onToggle={handToggleBookMark}
+                />
+            )}
         </div>
     );
 }
